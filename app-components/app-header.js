37c3bd6fe2e100a7926995860c656ac4964dce04
FrancesBrandia <francesrubinva@gmail.com>
2024-10-15 10:07:11 -0400

    Add files via upload

---
+++ app-components/app-header.js
@@ -0,0 +1,22 @@
+const appHeader = `
+    <div class="app-header">
+        <div class="app-header-logo">
+            <img class="logo" src="../../assets/icons/ACR-logo-light2.png" alt="ACR Logo">
+        </div>
+        <div class="app-header-triangle"></div>
+        <div class="app-header-content">
+            <div class="app-name">
+                Master Component Library
+            </div>
+        </div>
+    </div>
+`;
+
+class AppHeader extends HTMLElement {
+    constructor() {
+        super();
+        this.innerHTML = appHeader;
+    }
+}
+
+customElements.define('app-header', AppHeader);
